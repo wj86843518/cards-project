62db845a7793dee9913af566d0e8a2b1ff75844d
zhangyouwei <906461261@qq.com>
2021-01-21 15:06:59 -0800

    cardOne zhangyouwei

---
+++ src/views/yearGreeting/cardOne.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="box">
+    <div class="bg-box">    <van-image
+  width="100vw"
+  height="100vh"
+  fit="fill"
+  :src="require('../../assets/yearGreeting/cardOne/BG.gif')"
+/></div>
+  <div class="bg-word">    <van-image
+  width="100vw"
+  height="100vh"
+  fit="fill"
+  :src="require('../../assets/yearGreeting/cardOne/bgWord.gif')"
+/></div>
+</div>
+
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.html{
+  font-size: 12px;
+}
+.box{
+position: relative;
+ width: 100vw;
+  height: 100vh;
+}
+.bg-box{
+  position: absolute;
+    left: 0;
+    top: 0;
+  width: 100vw;
+  height: 100vh;  
+}
+.bg-word{
+    position: absolute;
+    left: 0;
+    top: 0;
+  width: 100vw;
+  height: 100vh;
+}
+
+</style>
